e40b85eec61d1c2cf1949007629fae285f9754ea
Kochetov <kochetov.yevhen.dev@gmail.com>
2023-11-30 18:16:41 +0200

    install material ui and create test component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Test from "./Test";
+
 function App() {
   return (
     <div>
-      Hello world
+      <Test />
     </div>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Test = () => {
+  return (
+    <div>
+      <Typography variant="h5" sx={{ color: "red", border: "1px solid black", borderRadius: 10 }}>
+        Hello world
+      </Typography>
+    </div>
+  );
+};
+
+export default Test;
